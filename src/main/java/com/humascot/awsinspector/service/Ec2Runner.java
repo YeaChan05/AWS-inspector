2c1f197517cdae37241c1a4e7bbb90dd84848cf6
ShinYeaChan <88381563+YeaChan05@users.noreply.github.com>
2023-07-04 22:13:21 +0900

    describe ec2 server cpu status

--- src/main/java/com/humascot/awsinspector/service/Ec2Runner.java
+++ src/main/java/com/humascot/awsinspector/service/Ec2Runner.java
@@ -1,13 +1,18 @@
 package com.humascot.awsinspector.service;
 
 import com.amazonaws.services.ec2.AmazonEC2;
-import com.amazonaws.services.ec2.model.MonitorInstancesRequest;
+import com.amazonaws.services.ec2.model.CpuOptions;
+import com.amazonaws.services.ec2.model.Instance;
+import com.amazonaws.services.ec2.model.Reservation;
 import com.humascot.awsinspector.config.AwsConfig;
 import com.humascot.awsinspector.config.AwsProfile;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Objects;
 
 /**
  * package :  com.humascot.awsinspector
@@ -21,10 +26,36 @@ public class Ec2Runner implements ApplicationRunner {
     
     private final AwsConfig awsConfig;
     private final AwsProfile awsProfile;
+    
     @Override
     public void run(ApplicationArguments args) throws Exception {
         AmazonEC2 ec2 = awsConfig.ec2Client();
-        MonitorInstancesRequest request=new MonitorInstancesRequest().withInstanceIds(awsProfile.getInstanceID());
-        ec2.monitorInstances(request);
+        
+        Instance runningInstance = getRunningInstance(ec2);
+        System.out.printf(
+                "Found instance with id %s, " +
+                        "AMI %s, " +
+                        "type %s, " +
+                        "state %s " +
+                        "and monitoring state %s",
+                Objects.requireNonNull(runningInstance).getInstanceId(),
+                runningInstance.getImageId(),
+                runningInstance.getInstanceType(),
+                runningInstance.getState().getName(),
+                runningInstance.getMonitoring().getState());
+        System.out.println();
+        CpuOptions cpuOptions = runningInstance.getCpuOptions();
+        System.out.println(cpuOptions.getAmdSevSnp());
+        System.out.println(cpuOptions.getThreadsPerCore());
+        System.out.println(cpuOptions.getCoreCount());
+    }
+    
+    private Instance getRunningInstance(AmazonEC2 ec2) {
+        List<Reservation> reservations = ec2.describeInstances().getReservations();
+        return reservations.stream()
+                .flatMap(reservation -> reservation.getInstances().stream())
+                .filter(instance -> instance.getMonitoring().getState().equals("enabled"))
+                .findFirst()
+                .orElse(null);
     }
 }
